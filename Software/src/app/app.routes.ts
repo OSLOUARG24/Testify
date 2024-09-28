c1d136db29003006f6992af3d459ff6f3c9eca57
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-09-28 01:59:37 -0300

    Update app.routes.ts
    Added User

--- Software/src/app/app.routes.ts
+++ Software/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { AuthGoogleComponent } from './auth-google/auth-google.component';
+import { UserComponent } from './user/user.component';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
 
 export const routes: Routes = [
-{ path: 'dashboard', component: AuthGoogleComponent },
+{ path: 'user', component: UserComponent },
+{ path: 'user/create', component: UpdateUserComponent },
+{ path: 'user/:id', component: UserComponent },
+{ path: 'user/edit/:id', component: UpdateUserComponent },
+{ path: 'dashboard', component: UserComponent },
 { path: '', component: AuthGoogleComponent, pathMatch: 'full' }
 ];
