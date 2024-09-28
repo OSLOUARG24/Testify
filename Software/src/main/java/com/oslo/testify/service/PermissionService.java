a3e8614d9f0f4fbd65a3619e45d587c6923256c9
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-09-28 01:12:38 -0300

    Services
    Permission, Project, RoleAssigment, RolePermission, Role, User

---
+++ Software/src/main/java/com/oslo/testify/service/PermissionService.java
@@ -0,0 +1,39 @@
+package com.oslo.testify.service;
+
+import com.oslo.testify.entity.Permission;
+import com.oslo.testify.repository.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PermissionService {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    public List<Permission> getAllPermissions() {
+        return permissionRepository.findAll();
+    }
+
+    public Permission savePermission(Permission permission) {
+        return permissionRepository.save(permission);
+    }
+
+    public Permission getPermissionById(Long id) {
+        return permissionRepository.findById(id).orElse(null);
+    }
+
+    public Permission updatePermission(Long id, Permission updatedPermission) {
+      Permission existingPermission = permissionRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Permiso no encontrado"));
+
+      existingPermission.setName(updatedPermission.getName());
+      return permissionRepository.save(existingPermission);
+    }
+
+    public void deletePermission(Long id) {
+      permissionRepository.deleteById(id);
+    }
+}
---
+++ Software/src/main/java/com/oslo/testify/service/UserService.java
@@ -0,0 +1,70 @@
+package com.oslo.testify.service;
+
+import com.oslo.testify.entity.User;
+import com.oslo.testify.repository.RoleAssigmentRepository;
+import com.oslo.testify.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleAssigmentRepository roleAssignmentRepository;
+
+    public UserService(UserRepository userRepository, RoleAssigmentRepository roleAssignmentRepository) {
+      this.userRepository = userRepository;
+      this.roleAssignmentRepository = roleAssignmentRepository;
+    }
+
+  public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+
+      boolean hasRoleAssignments  = roleAssignmentRepository.existsByUserId(id);
+
+      if (hasRoleAssignments) {
+        throw new RuntimeException("El usuario no puede ser eliminado porque está asociado a uno o más proyectos");
+      }
+
+      userRepository.deleteById(id);
+    }
+
+    public User getUserByEmail(String email) {
+      return  userRepository.getUserByEmail(email);
+    }
+
+  public User updateUser(Long id, User updatedUser) {
+    // Verificar si el usuario existe en la base de datos
+    Optional<User> existingUserOptional = userRepository.findById(id);
+
+    if (existingUserOptional.isPresent()) {
+      User existingUser = existingUserOptional.get();
+
+      // Actualizar los campos del usuario existente
+      existingUser.setName(updatedUser.getName());
+      existingUser.setEmail(updatedUser.getEmail());
+
+      // Guardar el usuario actualizado en la base de datos
+      return userRepository.save(existingUser);
+    } else {
+      throw new RuntimeException("Usuario no encontrado con id: " + id);
+    }
+  }
+}
