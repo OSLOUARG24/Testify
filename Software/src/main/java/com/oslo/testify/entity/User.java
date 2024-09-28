d0970eb1d80531d7c7dab72c518f0a1f15eb3b0b
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-09-28 00:49:14 -0300

    Clases User, Role y Permission

---
+++ Software/src/main/java/com/oslo/testify/entity/User.java
@@ -0,0 +1,84 @@
+package com.oslo.testify.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column( name = "name")
+    private String name;
+
+    @Column( name = "email")
+    private String email;
+
+    @Column( name = "admin_flag")
+    private Boolean isAdmin;
+
+    public User(Long id, String name, String email, Boolean isAdmin) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.isAdmin = isAdmin;
+    }
+
+    // Getters and Setters
+
+    public User(){}
+
+    public User(Long id, String name, String email, List<RoleAssigment> roles) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+    }
+
+  public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+  public Boolean getAdmin() {
+    return isAdmin;
+  }
+
+  public void setAdmin(Boolean admin) {
+    isAdmin = admin;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", email='" + email + '\'' +
+      ", isAdmin=" + isAdmin +
+      '}';
+  }
+}
