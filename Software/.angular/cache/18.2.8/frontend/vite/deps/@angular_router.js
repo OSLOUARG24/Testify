511798d76265300aeaec3a272cf88ae7f0c23b9b
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-10-19 13:30:37 -0300

    Ignore Angular folder v2

--- Software/.angular/cache/18.2.8/frontend/vite/deps/@angular_router.js
+++ Software/.angular/cache/18.2.8/frontend/vite/deps/@angular_router.js
@@ -1,7 +1,7 @@
 import {
   Title
-} from "./chunk-2SJ5PHNW.js";
-import "./chunk-GODU576P.js";
+} from "./chunk-ZNI5CPUP.js";
+import "./chunk-SRDAKOXQ.js";
 import {
   DOCUMENT,
   HashLocationStrategy,
@@ -10,7 +10,7 @@ import {
   LocationStrategy,
   PathLocationStrategy,
   ViewportScroller
-} from "./chunk-XNIX6GYB.js";
+} from "./chunk-W2E5K6UM.js";
 import {
   APP_BOOTSTRAP_LISTENER,
   APP_INITIALIZER,
@@ -77,12 +77,7 @@ import {
   ɵɵloadQuery,
   ɵɵqueryRefresh,
   ɵɵsanitizeUrlOrResourceUrl
-} from "./chunk-FNXF7C3G.js";
-import "./chunk-LPEPAQVY.js";
-import {
-  defer,
-  isObservable
-} from "./chunk-UEYFFO6E.js";
+} from "./chunk-LGTIMQHY.js";
 import {
   BehaviorSubject,
   ConnectableObservable,
@@ -97,10 +92,12 @@ import {
   concat,
   concatMap,
   defaultIfEmpty,
+  defer,
   filter,
   finalize,
   first,
   from,
+  isObservable,
   last,
   map,
   mapTo,
@@ -117,7 +114,7 @@ import {
   takeUntil,
   tap,
   throwError
-} from "./chunk-LBGRCHB7.js";
+} from "./chunk-6EXQV4WU.js";
 
 // node_modules/@angular/router/fesm2022/router.mjs
 var PRIMARY_OUTLET = "primary";
