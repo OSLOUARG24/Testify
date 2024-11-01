c034d3d565a1c6962c121e53c2a99769795c850a
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-11-01 17:51:44 -0300

    Desarrollo - Paginacion y Filtro Type
    Co-Authored-By: Levipichun <55004285+Levipichun@users.noreply.github.com>

--- Software/src/main/webapp/app/features/type/type.component.ts
+++ Software/src/main/webapp/app/features/type/type.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { ActivatedRoute, Router, RouterOutlet,RouterLinkActive,RouterLink  } from '@angular/router';
 import { Type } from './type.model';
 import { TypeService } from './type.service';
 import { User } from '../user/user.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteTypeComponent } from './delete-type/delete-type.component';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator'; // Aseg�rate de importar MatPaginatorModule correctamente
+import { ITEMS_PER_PAGE } from '../../app.constants';
+import { getCustomPaginatorIntl } from '../../custom-paginator-intl';
 
 @Component({
   selector: 'app-type',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatPaginatorModule],
   templateUrl: './type.component.html',
   styleUrls: ['./type.component.css']
 })
 export class TypeComponent implements OnInit {
   types: Type[] = [];
+  filteredTypes: Type[] = [];
+  paginatedTypes: Type[] = [];
   user: User = {};
+  pageSize = ITEMS_PER_PAGE;
+  pageIndex = 0;
+  totalItems = 0;
+  showSearch = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private typeService: TypeService,
               public dialog: MatDialog) {}
@@ -36,9 +47,33 @@ export class TypeComponent implements OnInit {
   loadTypes(): void {
     this.typeService.getTypes().subscribe((types: Type[]) => {
       this.types = types;
+      this.filteredTypes = types; // Inicializa con todas las tipos
+      this.totalItems = types.length;
+      this.updatePaginatedTypes();
     });
   }
 
+applyFilter(event: Event): void {
+  const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+  this.filteredTypes = this.types.filter(type =>
+    type.name?.toLowerCase().includes(filterValue) // Usa el operador de encadenamiento opcional (?.)
+  );
+  this.totalItems = this.filteredTypes.length;
+  this.pageIndex = 0; // Reinicia a la primera p�gina
+  this.updatePaginatedTypes();
+}
+
+updatePaginatedTypes(): void {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedTypes = this.filteredTypes.slice(startIndex, endIndex);
+  }
+
+onPageChange(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.updatePaginatedTypes();
+  }
 
   Cancel(): void {
     window.history.back();
@@ -61,5 +96,8 @@ export class TypeComponent implements OnInit {
     this.typeService.getTypes().subscribe(types => {
       this.types = types;
     });
-  }
+}
+ toggleSearch(): void {
+    this.showSearch = !this.showSearch;
+   }
 }
