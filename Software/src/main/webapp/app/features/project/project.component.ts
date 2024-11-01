6e0687197ee4bdac77ae0b8a3b26d939ef73976d
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-11-01 18:08:49 -0300

    Desarrollo - FixGitIgnore Edu
    Problema al cambiar de branch

--- Software/src/main/webapp/app/features/project/project.component.ts
+++ Software/src/main/webapp/app/features/project/project.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartData } from 'chart.js';
 import { ActivatedRoute } from '@angular/router';
 import { Project, ProjectStatus } from './project.model';
 import { ProjectService } from './project.service';
+import { BaseChartDirective } from 'ng2-charts';
 import { Router, RouterOutlet,RouterLinkActive,RouterLink } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteProjectComponent } from './delete-project/delete-project.component';
@@ -9,7 +11,7 @@ import { DeleteProjectComponent } from './delete-project/delete-project.componen
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, BaseChartDirective],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
@@ -18,6 +20,15 @@ export class ProjectComponent {
    projects?: Project[];
    projectId?: number ;
    projectStatus = ProjectStatus;
+   public pieChartOptions: ChartOptions = {
+           responsive: true,
+         };
+   public pieChartLabels: string[] = ['Pendiente', 'Aprobado', 'En Error'];
+   public pieChartData: ChartData<'pie', number[], string> = {
+       labels: this.pieChartLabels,
+       datasets: []
+     };
+   public pieChartType: ChartType = 'pie';  //line - pie - bar
 
    constructor(protected route: ActivatedRoute
               ,protected projectService: ProjectService
@@ -37,6 +48,7 @@ export class ProjectComponent {
          this.projects = [];
          this.getAllProjects();
        }
+       this.generateChartData();
    }
 
       getStatusDescription(status: ProjectStatus): string {
@@ -82,5 +94,43 @@ export class ProjectComponent {
                   this.projects = projects;
                 });
               }
+
+  generateChartData(): void {
+      const statusCounts = {
+        Pendiente: 0,
+        Aprobado: 0,
+        EnError: 0
+      };
+
+      console.log(JSON.stringify(this.projects));
+      // Recorre los iterations y testCases del proyecto
+      /*this.projects?.forEach((project: Project) => {
+            project.iterations?.forEach((iteration: Iteration) => {
+              iteration.stages?.forEach((stage: Stage) => {
+                  alert(stage.status);
+                  if (stage.status === StageStatus.PENDIENTE) {
+                    statusCounts.Pendiente++;
+                  } else if (stage.status === StageStatus.APROBADO) {
+                    statusCounts.Aprobado++;
+                  } else if (stage.status === StageStatus.ERROR) {
+                    statusCounts.EnError++;
+                  }
+              });
+            });
+          });*/
+        statusCounts.Aprobado++;
+        statusCounts.Aprobado++;
+        statusCounts.Pendiente++;
+      // Asigna los datos para el gráfico de torta
+      this.pieChartData = {
+            labels: this.pieChartLabels,
+            datasets: [
+              {
+                data: [statusCounts.Pendiente, statusCounts.Aprobado, statusCounts.EnError],
+                label: 'Estados de Pasos a Seguir'
+              }
+            ]
+          };
+    }
 
 }
