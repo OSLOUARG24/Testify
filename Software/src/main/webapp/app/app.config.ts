9e0aba5be4fa211e280d764802e71071b359db1c
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-11-01 14:14:46 -0300

    Desarrollo - Paginado en Categorias
    Se agrega el paginado en las categorias

--- Software/src/main/webapp/app/app.config.ts
+++ Software/src/main/webapp/app/app.config.ts
@@ -7,9 +7,12 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { getCustomPaginatorIntl } from './custom-paginator-intl';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: MatPaginatorIntl, useValue: getCustomPaginatorIntl() },
   provideCharts(withDefaultRegisterables()),
   provideRouter(routes), provideClientHydration(),provideHttpClient(), provideAnimationsAsync(),provideOAuthClient(), provideAnimationsAsync()]
 };
--- Software/src/main/webapp/app/features/category/category.component.ts
+++ Software/src/main/webapp/app/features/category/category.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { ActivatedRoute, Router, RouterOutlet,RouterLinkActive,RouterLink  } from '@angular/router';
 import { Category } from './category.model';
 import { CategoryService } from './category.service';
 import { User } from '../user/user.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteCategoryComponent } from './delete-category/delete-category.component';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator'; // Asegúrate de importar MatPaginatorModule correctamente
+import { ITEMS_PER_PAGE } from '../../app.constants';
+import { getCustomPaginatorIntl } from '../../custom-paginator-intl';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatPaginatorModule],
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
+  filteredCategories: Category[] = [];
+  paginatedCategories: Category[] = [];
   user: User = {};
+  pageSize = ITEMS_PER_PAGE;
+  pageIndex = 0;
+  totalItems = 0;
+  showSearch = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private categoryService: CategoryService
              ,public dialog: MatDialog) {}
@@ -33,10 +44,35 @@ export class CategoryComponent implements OnInit {
     }
   }
 
-  loadCategories(): void {
+loadCategories(): void {
     this.categoryService.getCategories().subscribe((categories: Category[]) => {
       this.categories = categories;
+      this.filteredCategories = categories; // Inicializa con todas las categorías
+      this.totalItems = categories.length;
+      this.updatePaginatedCategories();
     });
+  }
+
+applyFilter(event: Event): void {
+  const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+  this.filteredCategories = this.categories.filter(category =>
+    category.name?.toLowerCase().includes(filterValue) // Usa el operador de encadenamiento opcional (?.)
+  );
+  this.totalItems = this.filteredCategories.length;
+  this.pageIndex = 0; // Reinicia a la primera página
+  this.updatePaginatedCategories();
+}
+
+updatePaginatedCategories(): void {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedCategories = this.filteredCategories.slice(startIndex, endIndex);
+  }
+
+onPageChange(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.updatePaginatedCategories();
   }
 
   Cancel(): void {
@@ -60,5 +96,9 @@ export class CategoryComponent implements OnInit {
     this.categoryService.getCategories().subscribe(categories => {
       this.categories = categories;
     });
+  }
+
+ toggleSearch(): void {
+    this.showSearch = !this.showSearch;
   }
 }
