a95aeeae7daa16697001397ca226316b562a3bb3
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2024-12-01 03:26:56 -0300

    Desarrollo - Update Script_db.sql
    Se modifico la vista de category_status_v

--- Software/script_db.sql
+++ Software/script_db.sql
@@ -2,13 +2,13 @@ CREATE USER 'testify'@'localhost' IDENTIFIED BY 'testify'
 /
 GRANT ALL PRIVILEGES ON testify.* TO 'testify'@'localhost'
 /
-CREATE  VIEW `testify`.`project_approval_status` 
+CREATE  OR REPLACE VIEW `testify`.`project_approval_status` 
 AS select `p`.`id` AS `id`,coalesce(round(((sum((case when (`st`.`status` = 'APROBADO') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `approval_percentage` 
 from ((`testify`.`projects` `p` left join `testify`.`iterations` `it` on((`p`.`id` = `it`.`project_id`))) 
 left join `testify`.`stages` `st` on((`it`.`id` = `st`.`iteration_id`))) 
 where ((`st`.`previous_stage_id` is null) or ((`st`.`previous_stage_id` is not null) and (`st`.`status` = 'APROBADO'))) group by `p`.`id`
 /
-CREATE VIEW `testify`.`iteration_status_v` AS 
+CREATE OR REPLACE VIEW `testify`.`iteration_status_v` AS 
 select concat(`it`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS')) AS `id`,`pr`.`id` AS `project_id`,`pr`.`name` AS `project_name`
 ,`it`.`name` AS `iteration_name`
 ,coalesce(round(((sum((case when (`st`.`status` = 'PENDIENTE') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `pending_quantity`
@@ -19,8 +19,8 @@ join `testify`.`projects` `pr` on((`pr`.`id` = `it`.`project_id`)))
 where ((`st`.`previous_stage_id` is null) or ((`st`.`previous_stage_id` is not null) and (`st`.`status` = 'APROBADO'))) 
 group by `it`.`id`,`it`.`name` order by `pr`.`name`,`it`.`id`
 /
-CREATE VIEW `testify`.`category_status_v` AS 
-select concat(`it`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS')) AS `id`,`pr`.`id` AS `project_id`,`pr`.`name` AS `project_name`
+CREATE OR REPLACE VIEW `testify`.`category_status_v` AS 
+select md5(concat(it.id, '_', coalesce(st.status, 'NO_STATUS'), '_', cat.name)) AS `id`,`pr`.`id` AS `project_id`,`pr`.`name` AS `project_name`
 ,`cat`.`name` AS `category_name`
 ,coalesce(round(((sum((case when (`st`.`status` = 'PENDIENTE') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `pending_quantity`
 ,coalesce(round(((sum((case when (`st`.`status` = 'ERROR') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `error_quantity`
