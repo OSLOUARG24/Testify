8319d18bb16db57b3660a34b70cc19862990b3cd
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2025-07-07 00:48:35 -0300

    Fix admin user

--- Entregables/testify_db.sql
+++ Entregables/testify_db.sql
@@ -3,6 +3,7 @@ use testify;
 DROP TABLE category_status_v;
 
 CREATE OR REPLACE VIEW `testify`.`category_status_v` AS 
+<<<<<<< Updated upstream
 select md5(concat(`cat`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS'),'_',`cat`.`name`)) AS `id`
 ,`pr`.`id` AS `project_id`,`pr`.`name` AS `project_name`
 ,`cat`.`name` AS `category_name`
@@ -18,21 +19,36 @@ group by md5(concat(`cat`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS'),'_',`cat`
 ,`pr`.`id`,`pr`.`name`
 ,`cat`.`name`
 order by 3,1;
+=======
+SELECT md5(concat(`cat`.`id`,'_',`cat`.`name`)) AS `id`
+, `pr`.`id` AS `project_id`, `pr`.`name` AS `project_name`
+, `cat`.`name` AS `category_name`
+, coalesce(round(((sum((CASE WHEN (`st`.`status` = 'PENDIENTE') THEN 1 ELSE 0 END)) * 100.0) / NULLIF(COUNT(`st`.`id`),0)),2),0) AS `pending_quantity`
+, coalesce(round(((sum((CASE WHEN (`st`.`status` = 'ERROR') THEN 1 ELSE 0 END)) * 100.0) / NULLIF(COUNT(`st`.`id`),0)),2),0) AS `error_quantity`
+, coalesce(round(((sum((CASE WHEN (`st`.`status` = 'APROBADO') THEN 1 ELSE 0 END)) * 100.0) / NULLIF(COUNT(`st`.`id`),0)),2),0) AS `approved_quantity` 
+FROM `testify`.`iterations` `it` 
+LEFT JOIN `testify`.`stages` `st` ON `it`.`id` = `st`.`iteration_id`
+JOIN `testify`.`projects` `pr` ON `pr`.`id` = `it`.`project_id`
+JOIN `categories` `cat` ON `st`.`category_id` = `cat`.`id`
+WHERE ((`st`.`previous_stage_id` IS NULL) OR ((`st`.`previous_stage_id` IS NOT NULL) AND (`st`.`status` = 'APROBADO'))) 
+GROUP BY `cat`.`id`, `cat`.`name`, `pr`.`id`, `pr`.`name`
+ORDER BY `pr`.`name`, `cat`.`name`;
+>>>>>>> Stashed changes
 
 DROP TABLE iteration_status_v;
 
 CREATE OR REPLACE VIEW `testify`.`iteration_status_v` AS 
-select md5(concat(`it`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS'))) AS `id`,`pr`.`id` AS `project_id`,`pr`.`name` AS `project_name`
+select concat(`it`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS')) AS `id`
+,`pr`.`id` AS `project_id`
+,`pr`.`name` AS `project_name`
 ,`it`.`name` AS `iteration_name`
 ,coalesce(round(((sum((case when (`st`.`status` = 'PENDIENTE') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `pending_quantity`
 ,coalesce(round(((sum((case when (`st`.`status` = 'ERROR') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `error_quantity`
 ,coalesce(round(((sum((case when (`st`.`status` = 'APROBADO') then 1 else 0 end)) * 100.0) / nullif(count(`st`.`id`),0)),2),0) AS `approved_quantity` 
 from ((`testify`.`iterations` `it` left join `testify`.`stages` `st` on((`it`.`id` = `st`.`iteration_id`))) 
-join `testify`.`projects` `pr` on((`pr`.`id` = `it`.`project_id`))) 
+join `testify`.`projects` `pr` on((`pr`.`id` = `it`.`project_id`)))
 where ((`st`.`previous_stage_id` is null) or ((`st`.`previous_stage_id` is not null) and (`st`.`status` = 'APROBADO'))) 
-group by md5(concat(`it`.`id`,'_',coalesce(`st`.`status`,'NO_STATUS'))),`pr`.`id`,`pr`.`name`
-,`it`.`name`
-order by 3,4;
+group by `it`.`id`,`it`.`name` order by `pr`.`name`,`it`.`id`;
 
 
 DROP TABLE project_approval_status;
@@ -43,13 +59,19 @@ from ((`testify`.`projects` `p` left join `testify`.`iterations` `it` on((`p`.`i
 left join `testify`.`stages` `st` on((`it`.`id` = `st`.`iteration_id`))) 
 where ((`st`.`previous_stage_id` is null) or ((`st`.`previous_stage_id` is not null) and (`st`.`status` = 'APROBADO'))) group by `p`.`id`;
 
-INSERT INTO users VALUES (1,'slyeduardo@gmail.com',1,'Eduardo');
+INSERT INTO users VALUES (1,'osofia@uarg.unpa.edu.ar',1,'Albert');
 
 INSERT INTO `roles` VALUES (1,'GESTOR','Gestor de Pruebas'),(2,'TESTER','Tester'),(3,'GUEST','Invitado');
 
+<<<<<<< Updated upstream
 INSERT INTO `permissions` VALUES (22,'Usuarios'),(23,'Roles'),(24,'Permisos'),(25,'Tipos'),(26,'Subtipos'),(27,'Categorias'),(28,'Consultar Proyectos'),(29,'Consultar Iteraciones'),(30,'Modificar Iteraciones'),(31,'Eliminar Iteraciones'),(32,'Modificar Proyectos'),(33,'Eliminar Proyectos'),(34,'Exportar Proyectos'),(35,'Consultar Escenarios'),(36,'Modificar Escenarios'),(37,'Eliminar Escenarios'),(38,'Realizar Pruebas'),(39,'Crear Iteraciones'),(40,'Crear Proyectos'),(41,'Crear Escenarios'),(42,'Exportar Escenarios');
 
 INSERT INTO `role_permissions` VALUES (6,28,1),(20,28,3),(21,29,1),(22,35,1),(23,36,1),(24,37,1),(25,41,1),(26,42,1),(27,38,2),(28,29,3),(29,35,3),(30,34,1),(31,22,2),(33,24,2),(34,36,2);
+=======
+INSERT INTO `permissions` VALUES (22,'Usuarios'),(23,'Roles'),(24,'Permisos'),(25,'Tipos'),(26,'Subtipos'),(27,'Categorias'),(28,'Consultar Proyectos'),(29,'Consultar Iteraciones'),(30,'Modificar Iteraciones'),(31,'Eliminar Iteraciones'),(32,'Modificar Proyectos'),(33,'Eliminar Proyectos'),(34,'Exportar Proyectos'),(35,'Consultar Escenarios'),(36,'Modificar Escenarios'),(37,'Eliminar Escenarios'),(38,'Realizar Pruebas'),(39,'Crear Iteraciones'),(40,'Crear Proyectos'),(41,'Crear Escenarios'),(42,'Exportar Escenarios'),(43,'Copiar Escenarios');
+
+INSERT INTO `role_permissions` VALUES (6,28,1),(20,28,3),(21,29,1),(22,35,1),(23,36,1),(24,37,1),(25,41,1),(26,42,1),(27,38,2),(28,29,3),(29,35,3),(30,34,1),(31,22,2),(33,24,2),(34,36,2),(36,43,1);
+>>>>>>> Stashed changes
 
 INSERT INTO `types` VALUES (21,'Prueba Funcional'),(22,'Prueba de Integración'),(23,'Prueba de Regresión'),(24,'Prueba de Usabilidad'),(25,'Prueba de Seguridad'),(26,'Prueba de Compatibilidad'),(27,'Prueba de mantenimiento'),(28,'Prueba de Rendimiento'),(29,'Revisión Documento Clave'),(30,'Revisión documentos soporte'),(31,'Revisión Tecnica Formal (RTF)'),(32,'Pruebas de Calidad de Contenido');
 
